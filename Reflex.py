50639e197cd019d33384434717e167c3300d0b59
RETprojects <77463120+RETprojects@users.noreply.github.com>
2022-06-08 19:32:19 -0500

    Update Reflex.py
    I turned the sections of code supporting most of the gameplay into a function, playGame(), and began offering the player the option to play again by pressing B.

--- Reflex.py
+++ Reflex.py
@@ -62,6 +62,75 @@ def drawSquare(x: int, y: int):
     thumby.display.update()
     return
 
+def playGame():
+    # Once the button is pressed to start the game, display the name of the shape to look out for.
+    thumby.display.fill(0)
+    thumby.display.drawText("press A for", 5, 5, 1)
+    thumby.display.drawText("a " + str(shape_name), 5, 14, 1)
+    thumby.display.drawText("U to start", 5, 27, 1)
+    thumby.display.update()
+    
+    while True:
+        if thumby.buttonU.pressed():
+            break
+    
+    # Black out the screen, then flash a shape at random.
+    while True:
+        thumby.display.fill(0) # Fill canvas to black
+        thumby.display.update()
+        
+        # Generate a random number to determine whether to display a shape.
+        num = random.randint(0,100)
+        # If the random number is 0, draw a shape in the center of the screen.
+        if num == 0:
+            num = random.randint(0,2) # to decide which shape to display
+            if num == 0: # if 0, draw a circle
+                drawCircle(int((thumby.display.width/2) - (16/2)), int((thumby.display.height/2) - (16/2)))
+                if shape_name == "circle":
+                    beginStopwatch()
+                    break
+                else:
+                    time.sleep(1)
+            elif num == 1: # if 1, draw a triangle
+                drawTriangle(int((thumby.display.width/2) - (16/2)), int((thumby.display.height/2) - (16/2)))
+                if shape_name == "triangle":
+                    beginStopwatch()
+                    break
+                else:
+                    time.sleep(1)
+            else: # if 2, draw a square
+                drawSquare(int((thumby.display.width/2) - (16/2)), int((thumby.display.height/2) - (16/2)))
+                if shape_name == "square":
+                    beginStopwatch()
+                    break
+                else:
+                    time.sleep(1)
+    
+    # Get the time when the player reacts by pressing a button.
+    while True:
+        if thumby.buttonA.pressed():
+            end = time.ticks_ms()
+            thumby.display.fill(0) # get the shape off the screen
+            thumby.display.update()
+            break
+    
+    # Display the player's reaction time.
+    react_time = end - begin # the reaction time in ms
+    thumby.display.drawText("time:", 5, 5, 1)
+    thumby.display.drawText(str(react_time) + "ms", 5, 14, 1)
+    thumby.display.drawText("D to try again", 5, 23, 1)
+    thumby.display.drawText("B to quit", 5, 32, 1)
+    thumby.display.update()
+    
+    # Wait for the player to play again or quit the game.
+    while True:
+        if thumby.buttonD.pressed():
+            playGame() # play again
+        if thumby.buttonB.pressed():
+            sys.exit() # exit the program
+    
+    return
+
 # GAMEPLAY
 
 # Randomly determine which shape the player should react to.
@@ -86,69 +155,6 @@ thumby.display.update()
 
 while True:
     if thumby.buttonD.pressed():
-        break
-    if thumby.buttonB.pressed():
-        sys.exit() # exit the program
-
-# Once the button is pressed to start the game, display the name of the shape to look out for.
-thumby.display.fill(0)
-thumby.display.drawText("press A for", 5, 5, 1)
-thumby.display.drawText("a " + str(shape_name), 5, 14, 1)
-thumby.display.drawText("U to start", 5, 27, 1)
-thumby.display.update()
-
-while True:
-    if thumby.buttonU.pressed():
-        break
-
-# Black out the screen, then flash a shape at random.
-while True:
-    thumby.display.fill(0) # Fill canvas to black
-    thumby.display.update()
-    
-    # Generate a random number to determine whether to display a shape.
-    num = random.randint(0,100)
-    # If the random number is 0, draw a shape.
-    if num == 0:
-        num = random.randint(0,2) # to decide which shape to display
-        if num == 0: # if 0, draw a circle
-            drawCircle(int((thumby.display.width/2) - (16/2)), int((thumby.display.height/2) - (16/2)))
-            if shape_name == "circle":
-                beginStopwatch()
-                break
-            else:
-                time.sleep(1)
-        elif num == 1: # if 1, draw a triangle
-            drawTriangle(int((thumby.display.width/2) - (16/2)), int((thumby.display.height/2) - (16/2)))
-            if shape_name == "triangle":
-                beginStopwatch()
-                break
-            else:
-                time.sleep(1)
-        else: # if 2, draw a square
-            drawSquare(int((thumby.display.width/2) - (16/2)), int((thumby.display.height/2) - (16/2)))
-            if shape_name == "square":
-                beginStopwatch()
-                break
-            else:
-                time.sleep(1)
-
-# Get the time when the player reacts by pressing a button.
-while True:
-    if thumby.buttonA.pressed():
-        end = time.ticks_ms()
-        thumby.display.fill(0) # get the shape off the screen
-        thumby.display.update()
-        break
-
-# Display the player's reaction time.
-react_time = end - begin # the reaction time in ms
-thumby.display.drawText("time:", 5, 5, 1)
-thumby.display.drawText(str(react_time) + "ms", 5, 14, 1)
-thumby.display.drawText("B to quit", 5, 23, 1)
-thumby.display.update()
-
-# Wait for the player to quit the game.
-while True:
+        playGame()
     if thumby.buttonB.pressed():
         sys.exit() # exit the program
